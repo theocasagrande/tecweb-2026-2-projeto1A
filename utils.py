4fd1ba06b9f9f294d77c051fc696c3ba46df2888
theocasagrande <theozito.casagrande@gmail.com>
2026-08-27 08:11:34 -0300

    2- persistencia de dados

--- utils.py
+++ utils.py
@@ -1,5 +1,3 @@
-import json
-
 CONTENT_TYPES = {
     '.html': 'text/html',
     '.css': 'text/css',
@@ -19,17 +17,9 @@ def extract_route(request):
 def read_file(path):
     with open(path, 'rb') as f:
         return f.read()
-def load_data(filename):
-    with open(f'data/{filename}', 'r', encoding='utf-8') as f:
-        return json.load(f)
 def load_template(filename):
     with open(f'templates/{filename}', 'r', encoding='utf-8') as f:
         return f.read()
-def save_note(titulo, detalhes):
-    notes = load_data('notes.json')
-    notes.append({'titulo': titulo, 'detalhes': detalhes})
-    with open('data/notes.json', 'w', encoding='utf-8') as f:
-        json.dump(notes, f, ensure_ascii=False, indent=2)
 def build_response(body='', code=200, reason='OK', headers=''):
     if isinstance(body, str):
         body = body.encode()
--- views.py
+++ views.py
@@ -1,5 +1,8 @@
 from urllib.parse import unquote_plus
-from utils import load_data, load_template, build_response, save_note
+from utils import load_template, build_response
+from database import Database, Note
+
+db = Database('banco')
 
 def index(request):
     # A string de request sempre começa com o tipo da requisição (ex: GET, POST)
@@ -12,16 +15,16 @@ def index(request):
             chave, valor = chave_valor.split('=')
             params[chave] = unquote_plus(valor)
 
-        save_note(params['titulo'], params['detalhes'])
+        db.add(Note(title=params['titulo'], content=params['detalhes']))
 
         return build_response(code=303, reason='See Other', headers='Location: /')
 
-    # Cria uma lista de <li>'s para cada anotação
+    # Cria uma lista de cards para cada anotação
     # Se tiver curiosidade: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
     note_template = load_template('components/note.html')
     notes_li = [
-        note_template.format(title=dados['titulo'], details=dados['detalhes'])
-        for dados in load_data('notes.json')
+        note_template.format(title=nota.title, details=nota.content)
+        for nota in db.get_all()
     ]
     notes = '\n'.join(notes_li)
 
